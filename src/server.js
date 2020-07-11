f1e6d419cbfed01abc55ea815fccd19822815886
adams ip <adamsip2012@gmail.com>
2020-07-11 16:43:26 +0800

    okay

--- src/server.js
+++ src/server.js
@@ -28,80 +28,6 @@ var server = app.listen(2003);
 
 
 
-// var userSchema = mongoose.Schema({
-//     userID: {
-//         type: Number,
-//         required: true,
-//         unique: true
-//     },
-//     username: {
-//         type: String,
-//         required: true,
-//         unique: true
-//     },
-//     password: {
-//         type: String,
-//         required: true
-//     },
-//     favoriteEvent: {
-//         type: [Number],
-//         required: true
-//     } //,
-//     //icon: {
-//     //data: Buffer,
-//     //contentType: String
-//     //}
-// });
-
-// userSchema.plugin(autoIncrement.plugin, {
-//     model: 'User',
-//     field: 'userID'
-// })
-
-
-//image insert
-//module.exports = new mongoose.model('icon', userSchema);
-
-//app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(bodyParser.json());
-
-//var fs = require('fs');
-//var multer = require('multer');
-//var path = require('path');
-
-//var storage = multer.diskstorage({
-//destination: (req, file, cb) => {
-//cb(null, 'uploads')
-//},
-//filename: (req, file, cb) => {
-//cb(null, file.fieldname)
-//}
-//});
-
-//var imgModel = require('./model');
-
-//app.get('/', (req, res) => {
-//imgModel.find({}, (err, items) => {
-//if (err) {
-//console.log(err);
-//}
-//else {
-//res.render('app', { items: item });
-//}
-//});
-//});
-
-
-//code to create user account
-// var user = mongoose.model('User, userSchema');
-
-//app.use(multer({
-//dest: './2720proj-icons/',
-//rename: function(fieldname, filename) {
-//return filename;
-//}
-//}))
-
 app.get('/', function(req,res){
     res.sendFile(path.join(__dirname+"/index.html"));
     console.log(__dirname);
@@ -110,29 +36,4 @@ app.get('/', function(req,res){
 app.get('/hi', function(req,res){
     res.send("hello world");
 });
-// router.get('/', function(req,res){
-//     res.sendFile(path.join(__dirname+"/../src/index.html"));
-// });
 
-// router.get('/createAccount',function(req,res){
-//     res.sendFile(path.join(__dirname+"../src/createAC.html"))
-// });
-
-
-// app.use('/',router);
-// app.listen(process.env.port || 2009);
-
-
-// app.post('/insertUser', function(req, res) {
-
-//     var e = new user({
-//         username: req.body['username'],
-//         password: req.body['password'],
-//         $push: { favoriteEvent: -1 },
-//         //icon: {
-//         //data = fs.readfileSync(req.files.userphoto.path),
-//         //contentType = 'image/png'
-//         //}
-//     })
-
-// });
