2ca102c00068ff09c93613cf014c2d7bb62874c6
ChingKei <chingkei@pn-206-100.itsc.cuhk.edu.hk>
2020-07-14 11:18:28 +0800

    insert user code change

--- src/server.js
+++ src/server.js
@@ -2,16 +2,13 @@ var express = require('express');
 var app = express();
 const path = require('path');
 
-var autoIncrement = require('mongoose-auto-increment');
-autoIncrement.initialize(mongoose.connection);
-
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({
     extended: false
 }))
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://zxip5:x35197@localhost/zxip5');
+mongoose.connect('mongodb://thcheng6:x79714@localhost/thcheng6');
 
 var db = mongoose.connection;
 //upon connection failure
@@ -20,8 +17,6 @@ db.on('error', console.error.bind(console, 'Connection error'));
 db.once('open', function() {
     console.log("Connected succesfully");
 });
-
-var server = app.listen(2003);
 
 var userSchema = mongoose.Schema({
     userID: {
@@ -48,10 +43,60 @@ var userSchema = mongoose.Schema({
     //     //}
 });
 
-userSchema.plugin(autoIncrement.plugin, {
-    model: 'User',
-    field: 'userID'
+
+//code to create user account
+var user = mongoose.model('User, userSchema');
+
+//app.get('/', function(req, res) {
+//    res.sendFile(path.join(__dirname + "/index.html"));
+//    console.log(__dirname);
+//});
+
+// router.get('/', function(req,res){
+//     res.sendFile(path.join(__dirname+"/../src/index.html"));
+// });
+
+// router.get('/createAccount',function(req,res){
+//     res.sendFile(path.join(__dirname+"../src/createAC.html"))
+// });
+
+
+// app.use('/',router);
+// app.listen(process.env.port || 2009);
+
+
+app.post('/insertUser', function(req, res) {
+
+    var idMax;
+    user.findOne()
+        .sort('-eventID')
+        .exec(function(err, e) {
+            if (e == null) {
+                idMax = 0;
+            } else {
+                idMax = e.userID;
+            }
+            var f = new user({
+                userID: idMax + 1,
+                username: req.body['username'],
+                password: req.body['password'],
+                $push: { favoriteEvent: -1 }, //-1 means just initialized, upon insert new event, this element (element 0) will become '0', means some events are saved into it
+                //icon: {
+                //data = fs.readfileSync(req.files.userphoto.path),
+                //contentType = 'image/png'
+                //}
+            })
+            f.save(function(err) {
+                if (err)
+                    res.send(err);
+                res.send("User inserted.");
+            });
+        });
 });
+
+//listen to port x
+var server = app.listen(2019);
+
 
 
 //image insert
@@ -86,48 +131,9 @@ userSchema.plugin(autoIncrement.plugin, {
 //});
 //});
 
-
-//code to create user account
-var user = mongoose.model('User, userSchema');
-
 //app.use(multer({
 //dest: './2720proj-icons/',
 //rename: function(fieldname, filename) {
 //return filename;
 //}
 //}))
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + "/index.html"));
-    console.log(__dirname);
-});
-
-app.get('/hi', function(req, res) {
-    res.send("hello world");
-});
-// router.get('/', function(req,res){
-//     res.sendFile(path.join(__dirname+"/../src/index.html"));
-// });
-
-// router.get('/createAccount',function(req,res){
-//     res.sendFile(path.join(__dirname+"../src/createAC.html"))
-// });
-
-
-// app.use('/',router);
-// app.listen(process.env.port || 2009);
-
-
-app.post('/insertUser', function(req, res) {
-
-    var e = new user({
-        username: req.body['username'],
-        password: req.body['password'],
-        $push: { favoriteEvent: -1 },
-        //icon: {
-        //data = fs.readfileSync(req.files.userphoto.path),
-        //contentType = 'image/png'
-        //}
-    })
-
-});
