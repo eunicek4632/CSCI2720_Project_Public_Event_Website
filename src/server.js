86e7d0b5b0afac9d2b754a5a4d75c01596b106b6
ChingKei <chingkei@ChingKei-NB.local>
2020-07-14 19:44:24 +0800

    insert login code

--- src/server.js
+++ src/server.js
@@ -161,19 +161,34 @@ app.post('/insertComment', function(req, res) {
 
 
 function verify() {
-    var user = document.getElementById("username").value;
-    var pass = document.getElementById("pw").value;
-    if (user == '' || pass == '')
+    var username1 = document.getElementById("username").value;
+    var password1 = document.getElementById("pw").value;
+    if (username1 == '' || password1 == '')
         alert("You cannot leave any of the fields empty");
     else {
         //initiate HTTP request to verify user
-        $.ajax({
-                url: "",
-                type: "POST"
+        //$.ajax({
+        //url: "",
+        //type: "POST"
+        //})
+        //.done(function(txt) { // run if request is completed successfully
+        //$("#text").html(txt);
+        //})
+        user.findOne()
+            .where({ username: username1 })
+            .exec(function(err, e) {
+                if (e.password == password1) {
+                    alert("Logged in.");
+                } else {
+                    alert("Password incorrect.");
+                    document.getElementById("pw").value = "";
+                }
             })
-            .done(function(txt) { // run if request is completed successfully
-                $("#text").html(txt);
-            })
+            .catch(function(err) {
+                // handle error
+                alert("Username incorrect.");
+            });
+
     }
 }
 
