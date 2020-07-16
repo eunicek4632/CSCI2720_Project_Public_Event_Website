c71c5240d73aa342f66eb1f15e04e70fb3d11035
adams ip <adamsip2012@gmail.com>
2020-07-16 11:23:29 +0800

    handling login

--- src/server.js
+++ src/server.js
@@ -21,6 +21,10 @@ db.once('open', function() {
     console.log("Connected succesfully");
 });
 
+//
+// Data schema
+//
+
 var userSchema = mongoose.Schema({
     userID: {
         type: Number,
@@ -167,41 +171,6 @@ app.post('/insertComment', function(req, res) {
         });
 });
 
-
-
-
-function verify() {
-    var username1 = document.getElementById("username").value;
-    var password1 = document.getElementById("pw").value;
-    if (username1 == '' || password1 == '')
-        alert("You cannot leave any of the fields empty");
-    else {
-        //initiate HTTP request to verify user
-        //$.ajax({
-        //url: "",
-        //type: "POST"
-        //})
-        //.done(function(txt) { // run if request is completed successfully
-        //$("#text").html(txt);
-        //})
-        user.findOne()
-            .where({ username: username1 })
-            .exec(function(err, e) {
-                if (e.password == password1) {
-                    alert("Logged in.");
-                } else {
-                    alert("Password incorrect.");
-                    document.getElementById("pw").value = "";
-                }
-            })
-            .catch(function(err) {
-                // handle error
-                alert("Username incorrect.");
-            });
-
-
-    }
-}
 
 
 //image insert
