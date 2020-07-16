f43a1f130045f0521ed1de952f382bae01af66c8
adams ip <adamsip2012@gmail.com>
2020-07-16 23:10:02 +0800

    deploying login fix bug

--- src/operation.js
+++ src/operation.js
@@ -50,44 +50,46 @@ router.post('/login', function(req, res){
 	var username_input = req.body['username'];
 	var password_input = req.body['password'];
 
-	// try{
-	// 	var user = User.findOne({username_input});
+	console.log(username_input);
+	console.log(password_input);
+	try{
+		var user = User.findOne({username_input});
 
-	// 	if (!user) {
-	// 		var payload = {
-	// 			"success": 0,
-	// 			"message": "User Not Exist"
-	// 		}
-	// 		return res.status(400).send(payload);
-	// 	}
+		if (!user) {
+			var payload = {
+				"success": 0,
+				"message": "User Not Exist"
+			}
+			res.status(400).send(payload);
+		}
 
-	// 	var isMatch = (password_input == user.password);
+		var isMatch = (password_input == user.password);
 
-	// 	if (!isMatch) {
-	// 		var payload = {
-	// 			"success": 0,
-	// 			"message": "Incorret Password!"
-	// 		}
+		if (!isMatch) {
+			var payload = {
+				"success": 0,
+				"message": "Incorret Password!"
+			}
 
-	// 		return res.status(400).send(payload);
-	// 	}
+			res.status(400).send(payload);
+		}
 
-	// 	var payload = {
-	// 		"success": 0,
-	// 		"credential" : user.userID
-	// 	}
+		var payload = {
+			"success": 0,
+			"credential" : user.userID
+		}
 
-	// 	res.status(200).send(payload);
+		res.status(200).send(payload);
 
 
-	// }catch (e){
-	// 	console.error(e);
-	// 	var payload = {
-	// 		"success": 0,
-	// 		"message": "Server Error"
-	// 	}
-	// 	res.status(500).send(payload);
-	// }
+	}catch (e){
+		console.error(e);
+		var payload = {
+			"success": 0,
+			"message": "Server Error"
+		}
+		res.status(500).send(payload);
+	}
 
 
 });
