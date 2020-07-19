fb4ed2c9e4bd52dc9795658770bab7aebb7c21da
adams ip <adamsip2012@gmail.com>
2020-07-20 02:46:38 +0800

    button value toggle 13

--- src/operation.js
+++ src/operation.js
@@ -255,7 +255,7 @@ router.post('/likeEvent',async(req,res)=>{
 
         console.log( list.find(element => element == event_id));
 
-        if (list.find(element => element == event_id) != "undefined") {
+        if (list.find(element => element == event_id) == "undefined") {
             let doc = await User.findOneAndUpdate({userID: user_id}, {$push:{favoriteEvent:event_id}},{new: true});
             console.log('hi');
         }else{
