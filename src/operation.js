176979fbca2d4367ec54b79284520a507fa95e70
adams ip <adamsip2012@gmail.com>
2020-07-19 20:11:55 +0800

    fix big bug 3

--- src/operation.js
+++ src/operation.js
@@ -21,7 +21,7 @@ router.post('/reg', function(req, res) {
             username: req.body['username'],
             password: req.body['password'],
             userID: id,
-            favoriteEvent: [1]
+            favoriteEvent: []
         });
 
         user.save()
@@ -241,7 +241,7 @@ router.post('/likeEvent',async(req,res)=>{
     var user_id = req.query['userID']; 
     console.log('hi'+event_id+'bye'+user_id);
 
-    let doc = await User.findOneAndUpdate({userID: user_id}, {$push:{favoriteEvent:1}},{new: true});
+    let doc = await User.findOneAndUpdate({userID: user_id}, {$push:{favoriteEvent:event_id}},{new: true});
 
     console.log(doc.username);
 
